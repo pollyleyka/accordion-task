cec47f593abe8a5110c9ec58620d53451391e9d9
pollyleyka <polka321@rambler.ru>
2024-02-08 00:03:03 +0700

    add token pictures

--- src/Accordion/AccordionItem.tsx
+++ src/Accordion/AccordionItem.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react';
 import { CompanyType } from "../types/company";
+import big from './asserts/big.png';
+import small from './asserts/small.png';
+import mid from './asserts/mid.jpeg'
+import basic from './asserts/basic.jpg'
 
 interface IItemProps {
   title: string;
@@ -15,16 +19,24 @@ const AccordionItem: FC <IItemProps> = ({ title, company, isOpen, toggleAccordio
       <h2 className='accordion-title' onClick={toggleAccordion}>{title}</h2>
       {isOpen && (
         <section className='accordion-content'>
-          <div className='column'>
-            <picture>
-              <source media="(min-width: 992px)" srcSet={`${company.images['100x100']}`}/>
-              <source media="(min-width: 768px)" srcSet={`${company.images['64x64']}`}/>
-              <source media="(min-width: 576px)" srcSet={`${company.images['32x32']}`}/>
+          <div className='row'>
+            {/* <picture>
+              <source media="(min-width: 576px)" srcSet={company.images['32x32']}/>
+              <source media="(max-width: 992px)" srcSet={company.images['64x64']}/>
+              <source media="(min-width: 992px)" srcSet={company.images['100x100']}/>
               <img src={company.images['100x100']} width="100" height="100" alt={company.name}/>
-            </picture>
-      <span>Company: {company.name}</span>
+            </picture> */}
+            <picture>
+              <source type="image/png" media="(max-width: 576px)" srcSet={small}/>
+              <source type="image/jpeg" media="(max-width: 992px)" srcSet={mid}/>
+              <source type="image/jpeg" media="(min-width: 992px)" srcSet={big}/>
+              <img src={basic} width="100" height="100" alt={company.name}/>
+            </picture> 
+        <div className='column'>
+        <span>Company: {company.name}</span>
       <span>Employees: {company.employees}</span>
       <span>Date: {new Date(company.date * 1000).toLocaleDateString()}</span>
+        </div>
     </div>
         </section>
       )}
